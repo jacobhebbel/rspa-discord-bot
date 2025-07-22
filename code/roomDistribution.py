2dac357912dcae280b65a9761d635a9c78872218
jaocbhebbel <jacob.hebbel@gmail.com>
2025-07-22 15:12:40 -0400

    started a distribution function for secure rooms to prevent overuse of a single room. added notes for approach to implementing

--- code/roomDistribution.py
+++ code/roomDistribution.py
@@ -7,17 +7,23 @@ def findRoomForLesson(lesson, availableRooms):
 
     # getting datetime objects from lessons for intuitive comparisons
     lessonDatetime = util.lessonToDateTime(lesson)
-    lessonDate, lessonTime = lessonDatetime.date(), lessonDatetime.time()
+    lessonDate, lessonStart = lessonDatetime.date(), lessonDatetime.time()
     lessonDuration = time(minute=60) if lesson['isFullHour'] else time(minute=30)
     
     openRoom = None
     for room in availableRooms:
         if lessonDate in room.keys():
             # means this room has availability on this day
-
+            
             for time, duration in room:
-                if time < lessonTime and time + duration > lessonTime + lessonDuration:
+                # time is a time in the day and duration is how long the room is open from that time key
+
+                if time < lessonStart and time + duration > lessonStart + lessonDuration:
                     # means the room has an availability that covers the lesson start and duration
+
+                    # updating availableRooms to block out this lesson
+                    room.update({time: lessonStart - time})
+                    room.update({lessonStart + lessonDuration: duration - (lessonStart + lessonDuration)})
                     openRoom = room
                     break
     
@@ -41,7 +47,13 @@ def distributeConflictedLessons(availableRooms):
             # alert teacher that their lesson was secured
             # update lesson status to secured, assign room to lesson, update database
 
+def distributeSecuredLessons(availableRooms):
 
-
-    
-
+    securedLessons = getLessons(filter={'status': util.status['secured'], 'building': ''})
+    # plan: use a greedy algorithm for simple lesson distribution (guaranteed to work bc all lessons matching the filter don't conflict with each other)
+    # optimizations for evenly distributing room use: 
+    # -> make a variable Map[time][duration] = [available rooms] for a time complexity optimization
+    # -> count the hours of use remaining per day in each room; try to minimze variance of use across rooms
+    # -> distribute bigger lessons first bc they're harder to place
+    # -> count number of room uses in a day as another way to break ties
+    raise NotImplementedError

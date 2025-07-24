be18f89522dc9635cd1074a66e87b1e0c753775c
jaocbhebbel <jacob.hebbel@gmail.com>
2025-07-24 15:17:07 -0400

    put down comment outlining what needs to happen at this endpoint in more detail

--- code/server.py
+++ code/server.py
@@ -45,3 +45,15 @@ def getUserBookings():
 @server.route('/myLessons', methods=['GET'])
 def getUserLessons():
     raise NotImplementedError
+
+@server.route('/uploadSchedule', methods=['POST'])
+def uploadScheduleData():
+    data = request.get_json()
+    schedule = data['schedule']
+
+    # 1. reformat this data to be ready for db insertion
+    # 2. insert this data into the database as a new document for each date
+    # 3. give room assignments for conflicted lessons
+    # 4. notify every teacher in this pool of their lesson's updated status
+    # 5. distribute room assignments for secured lessons
+    # 6. notify every teacher in this pool of their lesson's new room assignment

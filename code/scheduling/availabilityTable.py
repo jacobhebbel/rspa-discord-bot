8b04a3478868f4a723d4c8bf8d18acc0e12fe2cf
jaocbhebbel <jacob.hebbel@gmail.com>
2025-07-27 10:22:56 -0400

    reworked index operator to be 1 arg (lesson dict)

--- code/scheduling/availabilityTable.py
+++ code/scheduling/availabilityTable.py
@@ -1,4 +1,4 @@
-import datetime
+import util
 class AvailabilityTable:
 
     def __init__(self, initialData):
@@ -14,41 +14,16 @@ class AvailabilityTable:
             self.data.insert({date: frame['rooms'] if frame['rooms'] else []})
 
     def __getitem__(self, index):
-        # index value is a tuple with a start time and duration
-        if not (isinstance(index, datetime) or isinstance(index, datetime.date)):
-            raise TypeError('Availability Table\'s first index must be a datetime object')
-        
-        if isinstance(index, datetime.date):
-            return self.data[index] if index in self.data.keys() else []
-        
-        return AvailabilityQuery(self.data, index)
-        
-        
-class AvailabilityQuery:
-
-    def __init__(self, data, index):
-        self.data = data
-        self.datetime = index
-    
-    def __getitem__(self, index):
-        
-        # for accessing room data for a specific day, and keeping the time slot structure
-        if index is None:
-            return self.data[self.datetime.date()]
-        
-        if not isinstance(index, datetime.time):
-            raise TypeError('Availability Table\'s second index must be a time object')
-
-        lessonDate, lessonStart = self.datetime.date(), self.datetime.time()
-        lessonDuration = index
-
+        # index is a lesson dict. return all rooms available to this lesson
+        lessonStart, lessonDuration = util.lessonToDateTime(index), util.durationToTimeDelta(index)
         availableRooms = []
-        for room, availabilities in self.data[lessonDate]:
-            for openingStart, openingDuration in availabilities:
 
-                if openingStart < (lessonStart + lessonDuration) < (openingStart + openingDuration):
-                    availableRooms.append(room)
-                    break
-
+        if lessonStart.date() not in self.data.keys():
+            return []
+        
+        rooms = self.data[lessonStart.date()]
+        for room, (start, duration) in rooms:
+            if (start < lessonStart) and (start + duration > lessonStart + lessonDuration):
+                availableRooms.append((room.split(' - ')))
+        
         return availableRooms
-

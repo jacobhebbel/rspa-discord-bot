aa813c135e05c791aa7a09efb008a86d833fae6d
jaocbhebbel <jacob.hebbel@gmail.com>
2025-08-04 14:07:32 -0400

    lesson obj passes all tests; didnt test compatibility w/ availability obj

--- code/scheduling/lesson.py
+++ code/scheduling/lesson.py
@@ -2,7 +2,7 @@ from datetime import datetime, date, time, timedelta
 class Lesson:
 
     def __init__(self, args: dict):
-        self.INVARIANT_FIELDS = ['teacherId', 'studentId', 'start', 'end', 'duration', 'location', 'status']
+        self.INVARIANT_FIELDS = ['id', 'teacherId', 'studentId', 'packageId', 'start', 'end', 'duration', 'location', 'status']
         
         '''
         assume a Lesson is only initialized from a database record.
@@ -17,36 +17,37 @@ class Lesson:
 
         for field, value in args.items():
             
-            if field is 'building' or field is 'room':
+            if field == 'building' or field == 'room':
                 self.location.append(value)
             
-            elif field is 'datetime':
+            elif field == 'datetime':
                 self.start = datetime.fromisoformat(value)
             
-            elif field is 'duration':
+            elif field == 'duration':
                 self.duration = timedelta(seconds=value)
             
             else:
                 setattr(self, field, value)
 
-        self.location = self.location.join(' - ')
+        self.location = (' - ').join(self.location)
         self.end = self.start + self.duration
-        
+
     def toDatabase(self):
         # turns into a str for db insertion
-        DB_FIELDS = ['teacherId', 'studentId', 'hasStudent', 'datetime', 'duration', 'building', 'room', 'hasRoom', 'status', 'isPackage', 'packageId']
+        DB_FIELDS = ['id', 'teacherId', 'studentId', 'hasStudent', 'datetime', 'duration', 'building', 'room', 'hasRoom', 'status', 'isPackage', 'packageId']
         
         return {
+            'id': self.id,
             'teacherId': self.teacherId,
             'datetime': datetime.isoformat(self.start),
             'duration': self.duration.seconds,
             'status': self.status,
             'building': self.location.split(' - ')[0],
             'room': self.location.split(' - ')[1],
-            'hasRoom': True if self.location is not '' else False,
+            'hasRoom': True if self.location != '' else False,
             'studentId': self.studentId,
-            'hasStudent': True if self.studentId is not '' else False,
-            'isPackage': True if self.packageId is not '' else False,
+            'hasStudent': True if self.studentId != '' else False,
+            'isPackage': True if self.packageId != '' else False,
             'packageId': self.packageId,
         }
     
@@ -56,10 +57,10 @@ class Lesson:
         
         return getattr(self, index)
         
-    def datetimeConflict(self, lesson):
+    def datetimeConflict(self, other):
         # returns t/f if this lesson conflicts with another
-        thisDate, otherDate = self.start.date(), lesson.start.date()
-        return (thisDate == otherDate) and (self.end > lesson.start or lesson.end > self.start)
+        thisDate, otherDate = self.start.date(), other.start.date()
+        return (thisDate == otherDate) and ((self.start < other.start and self.end > other.start) or (other.start < self.start and other.end > self.start))
 
     def roomConflict(self, lesson):
         # returns t/f if this lesson conflicts with another
--- code/tests/lesson.py
+++ code/tests/lesson.py
@@ -2,31 +2,151 @@ import util
 from datetime import datetime, timedelta
 from scheduling.lesson import Lesson
 
-lessonA = Lesson({
-    'start': datetime(2000, 1, 1, 8, 30),
-    'end': datetime(2000, 1, 1, 9),
-    'duration': timedelta(minutes=30)
-})
+'''
+Each lesson object is initialized with a dictionary similar to the result of calling mongo for a lesson document
+'''
+
+global lessonA
+global lessonB
+global lessonC
+
+lessonA, lessonB, lessonC = None, None, None
+
+
 
 
 def testInit():
-    raise NotImplementedError
+    global lessonA, lessonB, lessonC
+
+    lessonA = Lesson({
+    'id': '1',
+    'teacherId': 'jacob',
+    'studentId': '',
+    'hasStudent': False,
+    'packageId': '',
+    'isPackage': False,
+    'datetime': datetime(2000, 1, 1, 8, 30).isoformat(),
+    'duration': timedelta(minutes=30).seconds,
+    'building': 'West Hall',
+    'room': '323',
+    'hasRoom': True,
+    'status': util.status['secured'],
+    })
+
+    lessonB = Lesson({
+        'id': '2',
+        'teacherId': 'hope',
+        'studentId': '',
+        'hasStudent': False,
+        'packageId': '',
+        'isPackage': False,
+        'datetime': datetime(2000, 1, 1, 9).isoformat(),
+        'duration': timedelta(minutes=30).seconds,
+        'building': 'West Hall',
+        'room': '326',
+        'hasRoom': True,
+        'status': util.status['secured'],
+    })
+
+    lessonC = Lesson({
+        'id': '3',
+        'teacherId': 'Army',
+        'studentId': '',
+        'hasStudent': False,
+        'packageId': '',
+        'isPackage': False,
+        'datetime': datetime(2000, 1, 1, 8).isoformat(),
+        'duration': timedelta(minutes=60).seconds,
+        'building': 'West Hall',
+        'room': '323',
+        'hasRoom': True,
+        'status': util.status['pending'],
+    })
+
+    return isinstance(lessonA, Lesson) and isinstance(lessonB, Lesson) and isinstance(lessonC, Lesson)
 
 def testIndex():
-    raise NotImplementedError
+    global lessonA
+
+    expected = 'West Hall - 323'
+    observed = lessonA['location']
+    return expected == observed
+
 
 def testDatetimeConflict():
-    raise NotImplementedError
+    global lessonA, lessonB, lessonC
 
-def testFullConflict():
-    raise NotImplementedError
+    doesConflict, doesNotConflict = True, False
+
+    return (
+        (lessonA.datetimeConflict(lessonB) == doesNotConflict) and (lessonB.datetimeConflict(lessonA) == doesNotConflict) and 
+        (lessonA.datetimeConflict(lessonC) == doesConflict) and (lessonC.datetimeConflict(lessonA) == doesConflict)
+    )
+
+def testRoomConflict():
+    global lessonA, lessonB, lessonC
+
+    doesConflict, doesNotConflict = True, False
+
+    return (
+        (lessonA.roomConflict(lessonB) == doesNotConflict) and (lessonB.roomConflict(lessonA) == doesNotConflict) and
+        (lessonA.roomConflict(lessonC) == doesConflict) and (lessonC.roomConflict(lessonA) == doesConflict)
+    )
+
+def testToDatabase():
+    global lessonA, lessonB, lessonC
+    
+    dataA = {
+    'id': '1',
+    'teacherId': 'jacob',
+    'studentId': '',
+    'hasStudent': False,
+    'packageId': '',
+    'isPackage': False,
+    'datetime': datetime(2000, 1, 1, 8, 30).isoformat(),
+    'duration': timedelta(minutes=30).seconds,
+    'building': 'West Hall',
+    'room': '323',
+    'hasRoom': True,
+    'status': util.status['secured'],
+    }
+    dataB = {
+        'id': '2',
+        'teacherId': 'hope',
+        'studentId': '',
+        'hasStudent': False,
+        'packageId': '',
+        'isPackage': False,
+        'datetime': datetime(2000, 1, 1, 9).isoformat(),
+        'duration': timedelta(minutes=30).seconds,
+        'building': 'West Hall',
+        'room': '326',
+        'hasRoom': True,
+        'status': util.status['secured'],
+    }
+    dataC = {
+        'id': '3',
+        'teacherId': 'Army',
+        'studentId': '',
+        'hasStudent': False,
+        'packageId': '',
+        'isPackage': False,
+        'datetime': datetime(2000, 1, 1, 8).isoformat(),
+        'duration': timedelta(minutes=60).seconds,
+        'building': 'West Hall',
+        'room': '323',
+        'hasRoom': True,
+        'status': util.status['pending'],
+    }
+
+    return lessonA.toDatabase() == dataA and lessonB.toDatabase() == dataB and lessonC.toDatabase() == dataC
 
 def testFitsInside():
     raise NotImplementedError
 
 def main():
 
-    results = []
+    results = [testInit(), testIndex(), testDatetimeConflict(), testRoomConflict(), testToDatabase()]
     util.printTestResults(results)
 
 if __name__ == '__main__':
